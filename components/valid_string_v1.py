2726958cfdaaad1e7157cce9f60f4e327ecfa524
dreeaa <icyhope22@gmail.com>
2023-08-02 14:33:53 +1200

    created valid string v1

---
+++ components/valid_string_v1.py
@@ -0,0 +1,12 @@
+question = "enter your name "
+
+while True:
+    response = input(question)
+    x = response.isalpha()
+
+    if x == False:
+        print("input must only contain letters")
+    else:
+        print (response.title())
+        break
+    
